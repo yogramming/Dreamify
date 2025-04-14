7de41c56c2b47ecffc058d4e264d55dc0649f826
yogramming <yogeshdubey9336@gmail.com>
2025-04-14 14:05:36 +0530

    solve the port number issue

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def interpret():
         return jsonify({"reply": "Oops! Something went wrong while contacting Gemini."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
